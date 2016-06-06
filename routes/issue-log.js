9f963909281c76b8aeffd481d77f4ccf7c0ac790
Jackie Pham <jak464@bu.edu>
2016-06-05 20:26:45 -0400

    adding table for issues list

--- routes/issue-log.js
+++ routes/issue-log.js
@@ -1,7 +1,20 @@
 
+var connection =
+    require('./dbConnection.js').dbConnect();
+
+
 module.exports =
     function displayIssueLog(req, res){
-        res.render('issueLogView', {title: 'Project - Issue Log', issueLogSelected: 'pure-menu-selected',
-            user: req.user});
-    };
+        connection.query('SELECT * FROM Issues',
+            function(err,rows){
+                if(err) {
+                    console.log("Error Selecting : %s ", err);
+                }
+                res.render('issueLogView', {
+                    title: 'Project - Issue Log',
+                    issueLogSelected: 'pure-menu-selected',
+                    data: rows,
+                    user: req.user});
 
+            });
+    };
