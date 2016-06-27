490ce1a73145f7619883b04c87f9c74ae5e002bd
David Blair <dblair@thirdelement.com>
2016-06-26 23:11:32 -0400

    Dashboard display bugs assigned to user

--- index.js
+++ index.js
@@ -96,6 +96,20 @@ MomentHandler.registerHelpers(Handlebars);
 Filter.registerHelper(Handlebars);
 Handlebars.registerHelper('markdown', require('helper-markdown'));
 
+/* Helper to truncate text in rendering
+   https://gist.github.com/TastyToast/5053642 */
+Handlebars.registerHelper ('truncate', function (str, len) {
+    if (str && str.length > len && str.length > 0) {
+        var new_str = str + " ";
+        new_str = str.substr (0, len);
+        new_str = str.substr (0, new_str.lastIndexOf(" "));
+        new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+
+        return new Handlebars.SafeString ( new_str +'...' );
+    }
+    return str;
+});
+
 // static resources
 app.use(express.static(__dirname + '/public'));
 
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,19 @@
-var util = require('util');
+var connection = require('./dbConnection.js').dbConnect();
 
 module.exports =
     function displayHomePage(req, res){
+      var username = username = req.user ? req.user.username : "Anonymous"
+      connection.query('SELECT Issues.Id, Summary, IssueStatus, Priority, LastModifiedDate, AssignedTo, COUNT(IssueComments.IssueId) AS numComments FROM Issues LEFT JOIN IssueComments ON Issues.Id = IssueComments.IssueId WHERE IssueStatus != "Rejected" AND IssueStatus != "Closed" AND Archived != 1 AND AssignedTo = ? GROUP BY Issues.Id ORDER BY LastModifiedDate DESC',
+      username,
+      function(err,bugsData){
+        if(err) {
+          console.log("Error Selecting : %s ", err);
+        }
         res.render('homeView', {
             title: 'μProject | A Tiny Project Management Service',
             homeSelected: 'active',
+            css: ['bugs-log.css'],
+            bugs: bugsData,
             user: req.user});
-    };
+    });
+};
