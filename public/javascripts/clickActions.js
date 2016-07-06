277b1a8ff83ac9d150294a59389c9717bec7d259
Srivathsa Rajagopal <srivathsa.r@gmail.com>
2016-07-06 19:11:21 -0400

    Queued: Added Cancel button to Create Story Page. Cancel navigates to previous page. (This has to replaced by a better solution to hide the editing options)

--- public/javascripts/clickActions.js
+++ public/javascripts/clickActions.js
@@ -5,8 +5,13 @@ function addProject(){
     window.location.href = '/queued/add';
 }
 
+// This is cancel Create Project
 function cancelAdd(){
     window.location.href = '/queued';
+}
+
+function cancelCreateStory(){
+    window.history.back();
 }
 
 function cancelEditStory(){
