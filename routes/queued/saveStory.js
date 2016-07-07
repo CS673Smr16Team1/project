8cf8013acbd91b2d9f233452f23a648803e2f64d
sangDev <sangjlee@bu.edu>
2016-07-07 06:34:47 -0400

    added ability to assign due dates during create and update story

--- routes/queued/saveStory.js
+++ routes/queued/saveStory.js
@@ -8,6 +8,27 @@ module.exports =
     function saveStory(req , res , next){
 
         var projectId = req.params.projectId;
+        var date = new Date(req.body.datepicker);
+        var properlyFormatted = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getUTCDate()).slice(-2);
+        var maxPriority = 0;
+
+        console.log("ProjectID: %s",projectId);
+        // Get max priorityId from current Project for the specific story status
+        // Example SQL Query for projectId = 1 AND story_status = 'Backlog':
+        //     SELECT MAX(priorityId) FROM 673projectdev.QueuedStory WHERE projectId = 1 AND story_status = 'Backlog';
+        connection.query("SELECT MAX(priorityId) FROM QueuedStory WHERE projectId = ? AND story_status = ?",
+            [projectId, req.body.story_status],
+            function(err, returnedProrityId)
+            {
+                if (err)
+                    console.log("Error inserting Story: %s ",err );
+                console.log("priorityID: %s",returnedProrityId[0].priorityId);
+
+                maxPriority = returnedProrityId;
+            });
+
+        console.log(maxPriority[0]);
+
 
         var inputFromForm = {
             projectId: parseInt(projectId),
@@ -16,11 +37,14 @@ module.exports =
             story_status: req.body.story_status,
             assignee: req.body.assignedTo,   // need to update to user name of member projects
             type: req.body.type,
-            priority: req.body.priority
+            priority: req.body.priority,
+            priorityId: maxPriority + 1,
+            due_date: properlyFormatted
         };
 
         console.log ("testing...%s", req.body.priority);
-        console.log ("testing...");
+        console.log ("testing...%s", properlyFormatted);
+
 
         // #debug: printing projectId of the currently requested view
         console.log("projectId: %s",projectId);
@@ -33,7 +57,6 @@ module.exports =
                     console.log("Error inserting Story: %s ",err );
 
                 res.redirect('/queued/project/'+ projectId);
-
 
             });
 
--- routes/queued/viewStory.js
+++ routes/queued/viewStory.js
@@ -3,16 +3,88 @@
  */
 
 // connect to DB
-var connection =
-    require('../dbConnection.js').dbConnect();
+var connection = require('../dbConnection.js').dbConnect();
+var async = require("async");
+
 
 module.exports =
     function viewStory(req , res , next){
 
         var storyId = req.params.storyId;
         var projectId = req.params.projectId;
-        var project_name;
         console.log("projectId in Story : %s ", projectId);
+        console.log("storyID in Story : %s ", storyId);
+
+        async.series([
+                function(callback) {
+                    // Get all row with storyId = storyID
+                    connection.query('SELECT * FROM QueuedStory WHERE storyId = ?',
+                        storyId,
+                        function(err,rows) {
+                            console.log("rows : %s ", rows);
+                            return callback(null, rows);
+                        });
+                },
+                function(callback){
+                    // Get project_name and description with ProjectID = ID
+                    connection.query('SELECT project_name FROM QueuedProjects WHERE projectId = ?',
+                        projectId,
+                        function (err,projectName) {
+                            console.log("projectName : %s ", projectName[0]);
+                            return callback(null, projectName);
+                        });
+                },
+                function(callback){
+                    // Get rows where projectID and story_status is backlog
+                    connection.query('SELECT * FROM users',
+                        function (err,users) {
+                            console.log("users : %s ", users);
+                            return callback(null, users);
+                        });
+                }
+            ],
+
+            // callback to call StoryCreateView
+            function(err, results){
+
+                //results[0] - story
+                //results[1] - projectname
+                //results[2] - users
+                console.log("result[0] : %s ", results[0]);
+
+                var formatDateYYYYMMDD;
+                var formatDateMMDDYYYY;
+                var project_name = results[1][0].project_name;
+                var storyDetail = results[0];
+                var data = results[0][0];
+                var members = results[2];
+                console.log("storyDetail : %s ", storyDetail);
+                console.log("storyDetail : %s ", members);
+
+
+                date = new Date(storyDetail[0].due_date);
+                formatDateMMDDYYYY = ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + "-" + date.getFullYear();
+                    //06-30-2016
+                formatDateYYYYMMDD = date.getFullYear() + "-" +("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2);
+
+
+                res.render('queuedStoryView', {
+                        title: 'Queued | Project ' + project_name + ' Story Id: ' + storyId + ' | μProject',
+                        queuedSelected: 'active',
+                        project_name: project_name,
+                        css: ['bootstrap-markdown.css', 'queued-detail.css'],
+                        js: ['clickActions.js', 'bootstrap-markdown.js'],
+                        data: data,
+                        due_date_view: formatDateMMDDYYYY,
+                        due_date_edit: formatDateYYYYMMDD,
+                        user: req.user,
+                        members: members
+                    });
+
+
+
+            });
+/*
 
         connection.query('SELECT * FROM QueuedStory WHERE storyId = ?',
             storyId,
@@ -23,11 +95,18 @@ module.exports =
                         connection.query('SELECT * FROM users',
                             function (err, users) {
 
-                                console.log(rows);
                                 if (err) {
                                     console.log("Error Selecting : %s ", err);
                                 }
+                                console.log(rows);
+
                                 project_name = projectName[0].project_name;
+                                dateString = rows[0].due_date;
+                                console.log("dateString : %s ", dateString);
+
+                                date = new Date(dateString.due_date);
+                                properlyFormatted = ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + "-" + date.getFullYear();
+                                //06/30/2016
 
                                 res.render('queuedStoryView', {
                                     title: 'Queued | Project ' + project_name + ' Story Id: ' + storyId + ' | μProject',
@@ -36,6 +115,7 @@ module.exports =
                                     css: ['bootstrap-markdown.css', 'queued-detail.css'],
                                     js: ['clickActions.js', 'bootstrap-markdown.js'],
                                     data: rows[0],
+                                    due_date: properlyFormatted,
                                     user: req.user,
                                     members: users
                                 });
@@ -44,5 +124,7 @@ module.exports =
                     });
             });
 
+
+*/
 
     };
