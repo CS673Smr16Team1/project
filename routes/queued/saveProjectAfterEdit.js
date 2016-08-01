52caeeac6d955c2fc2266eb85398dfb235507fde
sangDev <sangjlee@bu.edu>
2016-08-01 12:23:52 -0400

    Queued: fixed bug: Edit Project > Last Member is automatically deleted when there are only 2 members in the project and one is removed.  Also member is not shown only one is assigned to the project

--- routes/queued/saveProjectAfterEdit.js
+++ routes/queued/saveProjectAfterEdit.js
@@ -8,8 +8,28 @@ var connection =
 module.exports =
     function saveProject(req , res , next){
         var id = req.params.projectId;
-        var project_members = JSON.stringify(req.body.members);
+
+        var memberList;
+
+        if (req.body.members !=  undefined){
+            // Bug#75: checking if req.body.member is array or not.
+            // Non-array is passed if there are one or less members assigned to the project.
+            if (Array.isArray(req.body.members) == false) {
+                // convert it into array
+                memberList = req.body.members.split(',');
+            } else{
+                // if an array assigned it to memberList for processing
+                memberList = req.body.members;
+            }
+            // Bug#75: fix completed
+        } else {
+            memberList = "";
+        }
+
+       // var project_members = JSON.stringify(req.body.members);
+        var project_members = JSON.stringify(memberList);
         console.log(req.body.project_owner);
+
         var inputFromForm = {
             project_name    :   req.body.project_name,
             Description     :   req.body.project_description,
